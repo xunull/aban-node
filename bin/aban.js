0f5008b48ed329a27ae2de21efd19949f025dc4a
xunull <todogo@vip.163.com>
2016-12-29 17:40:14 +0800

    调整方法为使用对象的方式

--- bin/aban.js
+++ bin/aban.js
@@ -1,32 +1,34 @@
 #!/usr/bin/env node
-var file_analysis = require('../lib/file_analysis');
-var util = require('../lib/util');
+var file_analysis = require('../lib/file_analysis')
+var util = require('../lib/util')
 var resultTable=require('../lib/result-table')
-var program = require('commander');
+var program = require('commander')
+var analysis = require('../lib')
 
 
 program
     .version(require('../package').version)
     .usage('[path]')
+    .option('-e,--extname <extname>','file extname')
+    .option('-s,--sort <sort>','sort result')
     .parse(process.argv)
 
-var targetPath=null;
+// console.dir(program)
+
+var targetPath=null
 
 if(0=== program.args.length) {
 	// 没有传入项目路径参数,默认分析当前路径
-	targetPath= process.cwd();
+	targetPath= process.cwd()
 } else {
 	// 分析给定的目录
-	targetPath= program.args[0];
+	targetPath= program.args[0]
 }
 
-console.log(util.readBanner());
-console.log('\n')
-let result = file_analysis(targetPath);
-
-// console.dir(result,{
-// 	colors:true,
-// 	depth:8
-// })
-
-console.log(resultTable.getTable(result.extnameFileCountMap,result.extnameLineCountMap))
+// 统计指定格式并且排序
+// 不过如果只统计一种语言的话,一般都是想看文件的分布
+if(program.extname) {
+    analysis.consoleTableResultSomeExt(targetPath,program.extname)
+} else {
+    analysis.consoleBasic(targetPath)
+}
