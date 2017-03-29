8a74827407ef4641a0c88a5913f043e28c5eca89
xunull <todogo@vip.163.com>
2017-03-29 15:20:43 +0800

    add some snippets

--- README.md
+++ README.md
@@ -1,7 +1,32 @@
 # aban-node
 代码统计工具
 
-# usage
-**npm i -g aban**
+## install
+`npm i -g aban**`
 
-在目标目录下执行aban就可,或者给定一个绝对路径参数
+## usage
+
+在目标目录下执行 `aban`
+
+或者指定一个绝对路径，如 `aban /home/some_user/some_project`
+
+默认统计的文件为
+
+- .java
+- .js
+- .html
+- .css
+- .json
+- .yaml
+- .c
+- .py
+- .go
+- .cpp
+- .lua
+- .pl
+- .php
+- .vue
+
+使用 **-e** 选项可以指定统计的类型
+如 `aban somepath -e .js,.css`
+多种类型的文件扩展名间用逗号分隔
--- bin/aban.js
+++ bin/aban.js
@@ -1,34 +1,50 @@
 #!/usr/bin/env node
-var file_analysis = require('../lib/file_analysis')
-var util = require('../lib/util')
-var resultTable=require('../lib/result-table')
-var program = require('commander')
-var analysis = require('../lib')
+const program = require('commander');
+const analysis = require('../lib');
 
+function range(val) {
+    return val.split('..').map(Number);
+}
+
+function collect(val, memo) {
+    memo.push(val);
+    return memo;
+}
+
+function increaseVerbosity(v, total) {
+    return total + 1;
+}
+
+function list(val) {
+    return val.split(',');
+}
 
 program
     .version(require('../package').version)
     .usage('[path]')
-    .option('-e,--extname <extname>','file extname')
-    .option('-s,--sort <sort>','sort result')
-    .parse(process.argv)
+    .option('-e,--extname <extname>', 'file extname', list)
+    .option('-s,--sort <sort>', 'sort result')
+    .parse(process.argv);
 
 // console.dir(program)
 
-var targetPath=null
+let targetPath = null;
 
-if(0=== program.args.length) {
-	// 没有传入项目路径参数,默认分析当前路径
-	targetPath= process.cwd()
+if (0 === program.args.length) {
+    // 没有传入项目路径参数,默认分析当前路径
+    targetPath = process.cwd();
 } else {
-	// 分析给定的目录
-	targetPath= program.args[0]
+    // 分析给定的目录
+    targetPath = program.args[0];
 }
+
+// console.log(program.args);
+// console.log(program.extname);
 
 // 统计指定格式并且排序
 // 不过如果只统计一种语言的话,一般都是想看文件的分布
-if(program.extname) {
-    analysis.consoleTableResultSomeExt(targetPath,program.extname)
+if (program.extname) {
+    analysis.consoleTableResultSomeExt(targetPath, ...program.extname);
 } else {
-    analysis.consoleBasic(targetPath)
+    analysis.consoleDefault(targetPath);
 }
